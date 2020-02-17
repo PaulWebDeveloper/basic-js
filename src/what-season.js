3b0462ee71e2f6f45d54e916d680f4f5e32008c9
Paul <paulworkweb@gmail.com>
2020-02-17 17:50:54 +0300

    feat: implement what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,15 @@
-module.exports = function getSeason(/* date */) {
+module.exports = function getSeason(date) {
+  if (!date) return 'Unable to determine the time of year!';
+  
+  let timeOfYear = ['winter', 'spring', 'summer', 'autumn'];
+  let value = JSON.stringify(date);
+
+  if (value.length !== 26) throw new Error('Invalid argument');
+
+  value = value.split('-')[1];
+  value = Math.floor(value / 3);
+  if (value === 4) value = 0;
+  
+  return timeOfYear[value];
   throw 'Not implemented';
-  // remove line with error and write your code here
 };
