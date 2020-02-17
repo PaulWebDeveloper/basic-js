6c0a627bdcb34dc3d0dea6512aea59f652639642
Paul <paulworkweb@gmail.com>
2020-02-17 11:21:12 +0300

    feat: implement dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,15 @@
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  members = clearUnnecessary(members);
+  members = searchDreamTeamName(members);
+
+  return members;
   throw 'Not implemented';
-  // remove line with error and write your code here
 };
+
+const clearUnnecessary = arr => arr.map(item => 
+  typeof item === 'string' ? item.toUpperCase().replace(/\s/g, '') : ' ').sort();
+
+const searchDreamTeamName = arr => arr.map(item => 
+  item.split('').filter((elem, i) => /^[A-Z]/.test(elem) && !i ? item : '')).join('');
