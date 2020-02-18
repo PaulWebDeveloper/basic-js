29b0c7a174576d7c1d99935adbd64ac382968c28
Paul <paulworkweb@gmail.com>
2020-02-18 10:59:49 +0300

    feat: implement transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,27 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) throw new Error('if arr is not an Array');
+
+  let count = 1;
+
+  arr.filter(item => item === '--discard-next' || item === '--discard-prev' ? count++ : '');
+
+  for (let i = 0, len = arr.length; i < len; i++) {
+    if (arr[i] === '--double-next') i + 1 < len ? arr[i] = arr[i+1] : arr[i] = ' ';
+    if (arr[i] === '--double-prev') i ? arr[i] = arr[i-1] : arr[i] = ' ';
+  }
+
+  while (count--) {
+    for (let i = 0, len = arr.length; i < len; i++) {
+      if (arr[i] === '--discard-next' || arr[i] === '--discard-prev') {
+        if (arr[i] === '--discard-next' && i + 1 < len) arr[i+1] = ' ';
+        if (arr[i] === '--discard-prev' && i) arr[i-1] = ' ';
+        arr[i] = ' ';
+        break;
+      }
+    }
+    arr = arr.filter(item => item !== ' ');
+  }
+
+  return arr;
+  throw 'Not implemented';
 };
