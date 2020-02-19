c3f1d1d826d959e9d249abd92005a5fc5ae789d1
Paul <paulworkweb@gmail.com>
2020-02-19 14:18:25 +0300

    feat: implement simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,23 +1,28 @@
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.push(value);
+    return this;
   },
   removeLink(position) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    if (position > 0 && position <= this.chain.length) {
+      this.chain.splice(position - 1, 1);
+      return this;
+    }
+    this.chain = [];
+    throw Error('wrong link');
   },
   reverseChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+    let result = this.chain.map(item => '( ' + item + ' )').join('~~');
+    this.chain = [];
+    return result;
   }
 };
 
